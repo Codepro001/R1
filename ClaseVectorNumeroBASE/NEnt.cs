00b96eaef257e0bedbe3b6f96988f871f4c28130
Codepro001 <codeficct@gmail.com>
2024-11-15 18:43:05 -0400

    Add files via upload

---
+++ ClaseVectorNumeroBASE/NEnt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examensb
+{
+    class NEnt
+    {
+    
+        private int n;
+
+
+        public NEnt()
+        {
+            n = 0;
+        }
+
+
+        public void cargar(int dato)
+        {
+            n = dato;
+        }
+
+
+
+        
+    }
+}
